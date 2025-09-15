28ac3641b9f7e71040a2b6075af50e4efc1339a5
Ron <ron@ronsbrain.com>
2025-09-15 19:36:03 -0400

    Initial triangle tiling

--- src/game.rs
+++ src/game.rs
@@ -1,34 +1,11 @@
 use glam::{Vec2, Vec3};
 use std::collections::VecDeque;
 
-const SQRT_3_OVER_4: f32 = 1.732050807568877293527446341505872367 / 4.;
+const SQRT_3_OVER_4: f32 = 1.732_050_8 / 4.;
 
 pub enum Command {
     Clear(Vec3),
     RenderLine((Vec2, Vec2, Vec3)),
-}
-
-#[derive(Clone)]
-enum EdgeType {
-    TopLeft,
-    TopRight,
-    Bottom,
-    Top,
-    BottomLeft,
-    BottomRight,
-}
-
-impl EdgeType {
-    fn opposite(&self) -> Self {
-        match self {
-            EdgeType::TopLeft => EdgeType::BottomRight,
-            EdgeType::TopRight => EdgeType::BottomLeft,
-            EdgeType::Bottom => EdgeType::Top,
-            EdgeType::Top => EdgeType::Bottom,
-            EdgeType::BottomLeft => EdgeType::TopRight,
-            EdgeType::BottomRight => EdgeType::TopLeft,
-        }
-    }
 }
 
 #[derive(Clone)]
@@ -47,174 +24,142 @@ impl TriangleOrientation {
 }
 
 #[derive(Clone)]
-struct Edge {
-    p1: Vec2,
-    p2: Vec2,
-    color: Vec3,
-}
-
-impl Edge {
-    fn new(x1: f32, y1: f32, x2: f32, y2: f32, color: Vec3) -> Self {
-        /* Ensure edges are always defined left to right WRT x */
-        let (p1, p2) = match x1 < x2 {
-            true => {
-                (Vec2::new(x1, y1), Vec2::new(x2, y2))
-            }
-            false => {
-                (Vec2::new(x2, y2), Vec2::new(x1, y1))
-            }
-        };
-
-        Self {
-            p1,
-            p2,
-            color,
-        }
-    }
-}
-
-#[derive(Clone)]
 struct Tile {
     center: Vec2,
-    edges: Vec<(Edge, EdgeType)>,
-    points: Vec<Vec2>,
+    clockwise_points: Vec<Vec2>,
     orientation: TriangleOrientation,
+    original_side_size: f32,
 }
 
 impl Tile {
+    fn edges(&self) -> Vec<(Vec2, Vec2)> {
+        let mut edges: Vec<(Vec2, Vec2)> = self
+            .clockwise_points
+            .windows(2)
+            .map(|e| (e[0], e[1]))
+            .collect();
+        edges.push((
+            self.clockwise_points[self.clockwise_points.len() - 1],
+            self.clockwise_points[0],
+        ));
+        edges
+    }
+
     fn neighboring_positions(&self) -> Vec<Vec2> {
-        self.edges
-            .iter()
-            .map(|(e, et)| {
-                let side_size = e.p1.distance(e.p2);
-                match et {
-                    EdgeType::TopLeft => Vec2::new(e.p1.x, self.center.y),
-                    EdgeType::TopRight => Vec2::new(e.p2.x, self.center.y),
-                    EdgeType::Bottom => Vec2::new(self.center.x, e.p1.y - side_size * SQRT_3_OVER_4),
-                    EdgeType::Top => Vec2::new(self.center.x, e.p1.y + side_size * SQRT_3_OVER_4),
-                    EdgeType::BottomLeft => Vec2::new(e.p1.x, self.center.y),
-                    EdgeType::BottomRight => Vec2::new(e.p2.x, self.center.y),
-                }
-            })
-            .collect()
+        match self.orientation {
+            TriangleOrientation::Up => {
+                vec![
+                    Vec2::new(self.clockwise_points[0].x, self.center.y),
+                    Vec2::new(self.clockwise_points[2].x, self.center.y),
+                    Vec2::new(
+                        self.center.x,
+                        self.clockwise_points[0].y - self.original_side_size * SQRT_3_OVER_4,
+                    ),
+                ]
+            }
+            TriangleOrientation::Down => {
+                vec![
+                    Vec2::new(
+                        self.center.x,
+                        self.clockwise_points[0].y + self.original_side_size * SQRT_3_OVER_4,
+                    ),
+                    Vec2::new(self.clockwise_points[0].x, self.center.y),
+                    Vec2::new(self.clockwise_points[1].x, self.center.y),
+                ]
+            }
+        }
+    }
+
+    fn contains_point(&self, point: Vec2) -> bool {
+        if self.center.distance_squared(point) > self.original_side_size.powi(2) * 2. {
+            return false;
+        }
+        let ab = (self.clockwise_points[0] - self.clockwise_points[1]).extend(0.);
+        let bc = (self.clockwise_points[1] - self.clockwise_points[2]).extend(0.);
+        let ca = (self.clockwise_points[2] - self.clockwise_points[0]).extend(0.);
+        let ap = (self.clockwise_points[0] - point).extend(0.);
+        let bp = (self.clockwise_points[1] - point).extend(0.);
+        let cp = (self.clockwise_points[2] - point).extend(0.);
+
+        let h = ab.cross(ap).z;
+        let i = bc.cross(bp).z;
+        let j = ca.cross(cp).z;
+
+        (h.is_sign_positive() && i.is_sign_positive() && j.is_sign_positive())
+            || (h.is_sign_negative() && i.is_sign_negative() && j.is_sign_negative())
     }
 }
 
 struct Tiles {
     side_size: f32,
-    edges: Vec<Edge>,
     tiles: Vec<Tile>,
     tile_queue: VecDeque<Tile>,
-    color_sequence: std::iter::Cycle<std::vec::IntoIter<Vec3>>,
 }
 
 impl Tiles {
     fn new(side_size: f32) -> Self {
         let mut result = Self {
             side_size,
-            edges: Vec::new(),
             tiles: Vec::new(),
             tile_queue: VecDeque::new(),
-            color_sequence: vec![
-                Vec3::new(1., 1., 1.),
-                Vec3::new(1., 1., 0.),
-                Vec3::new(1., 0., 1.),
-                Vec3::new(0., 1., 1.),
-                Vec3::new(1., 0., 0.),
-                Vec3::new(0., 1., 0.),
-                Vec3::new(1., 0., 0.),
-            ].into_iter().cycle(),
         };
 
-        let first = result.triangle_at(Vec2::new(0., 0.), TriangleOrientation::Up);
+        let first = result.make_triangle_at(Vec2::new(0., 0.), TriangleOrientation::Up);
         result.tile_queue.push_back(first);
 
         result
     }
 
-    fn triangle_at(&mut self, point: Vec2, orientation: TriangleOrientation) -> Tile {
+    fn make_triangle_at(&mut self, point: Vec2, orientation: TriangleOrientation) -> Tile {
         let left = point.x - self.side_size * 0.5;
         let right = point.x + self.side_size * 0.5;
         let top = point.y + self.side_size * SQRT_3_OVER_4;
         let bottom = point.y - self.side_size * SQRT_3_OVER_4;
 
-        let color = self.color_sequence.next().unwrap();
-        let (point1, point2, point3, edge1, edge2, edge3) = match orientation {
-            TriangleOrientation::Up => (
-                Vec2::new(left, bottom),
-                Vec2::new(point.x, top),
-                Vec2::new(right, bottom),
-                Edge::new(left, bottom, point.x, top, color),
-                Edge::new(point.x, top, right, bottom, color),
-                Edge::new(left, bottom, right, bottom, color),
-            ),
-            TriangleOrientation::Down => (
-                Vec2::new(left, top),
-                Vec2::new(right, top),
-                Vec2::new(point.x, bottom),
-                Edge::new(left, top, right, top, color),
-                Edge::new(right, top, point.x, bottom, color),
-                Edge::new(point.x, bottom, left, top, color),
-            )
-        };
-        
-        let edges = match orientation {
-            TriangleOrientation::Up => {
-                vec![(edge1, EdgeType::Bottom), (edge2, EdgeType::TopRight), (edge3, EdgeType::TopLeft)]
-            }
-            TriangleOrientation::Down => {
-                vec![(edge1, EdgeType::Top), (edge2, EdgeType::BottomRight), (edge3, EdgeType::BottomLeft)]
-            }
-        };
-
-        let tile = Tile {
-            center: point,
-            points: vec![point1, point2, point3],
-            edges,
-            orientation,
-        };
-
-        tile
+        match orientation {
+            TriangleOrientation::Up => Tile {
+                center: point,
+                clockwise_points: vec![
+                    Vec2::new(left, bottom),
+                    Vec2::new(point.x, top),
+                    Vec2::new(right, bottom),
+                ],
+                original_side_size: self.side_size,
+                orientation,
+            },
+            TriangleOrientation::Down => Tile {
+                center: point,
+                clockwise_points: vec![
+                    Vec2::new(left, top),
+                    Vec2::new(right, top),
+                    Vec2::new(point.x, bottom),
+                ],
+                original_side_size: self.side_size,
+                orientation,
+            },
+        }
     }
 
     fn process_tile_queue(&mut self) {
         if let Some(tile) = self.tile_queue.pop_front() {
-            for (edge, _edge_type) in tile.edges.iter() {
-                self.edges.push(edge.clone());
+            for position in tile.neighboring_positions() {
+                if position.x > -1.
+                    && position.x < 1.
+                    && position.y > -1.
+                    && position.y < 1.
+                    && !self
+                        .tiles
+                        .iter()
+                        .chain(self.tile_queue.iter())
+                        .any(|t| t.contains_point(position))
+                {
+                    let tile = self.make_triangle_at(position, tile.orientation.opposite());
+                    self.tile_queue.push_back(tile);
+                }
             }
 
             self.tiles.push(tile.clone());
-
-            for position in tile.neighboring_positions() {
-                if position.x > -1. && position.x < 1. && position.y > -1. && position.y < 1. {
-                    if self.tile_at(position).is_none() {
-                        let tile = self.triangle_at(position, tile.orientation.opposite());
-                        self.tile_queue.push_back(tile);
-                    }
-                }
-            }
         }
-    }
-
-    fn tile_at(&self, position: Vec2) -> Option<&Tile> {
-        for tile in self.tiles.iter() {
-            let ab = (tile.points[0] - tile.points[1]).extend(0.);
-            let bc = (tile.points[1] - tile.points[2]).extend(0.);
-            let ca = (tile.points[2] - tile.points[0]).extend(0.);
-            let ap = (tile.points[0] - position).extend(0.);
-            let bp = (tile.points[1] - position).extend(0.);
-            let cp = (tile.points[2] - position).extend(0.);
-
-            let h = ab.cross(ap).z;
-            let i = bc.cross(bp).z;
-            let j = ca.cross(cp).z;
-
-            if (h.is_sign_positive() && i.is_sign_positive() && j.is_sign_positive()) ||
-                (h.is_sign_negative() && i.is_sign_negative() && j.is_sign_negative()) {
-                    return Some(tile);
-            }
-        }
-        return None;
     }
 }
 
@@ -225,28 +170,27 @@ pub struct Game {
 
 impl Game {
     pub fn new() -> Self {
-        let mut tiles = Tiles::new(0.1);
-        tiles.triangle_at(Vec2::new(0., 0.), TriangleOrientation::Up);
-        Self {
-            tiles,
-            ticks: 0,
-        }
+        let tiles = Tiles::new(0.1);
+        Self { tiles, ticks: 0 }
     }
 
     pub fn tick(&mut self, mut command_arena: Vec<Command>) -> Vec<Command> {
         use Command::*;
 
+        self.tiles.process_tile_queue();
+
         self.ticks += 1;
-        if self.ticks > 25 {
+        if self.ticks > 50 {
             self.ticks = 0;
-            self.tiles.process_tile_queue();
         }
 
         command_arena.clear();
         command_arena.push(Clear(Vec3::new(0., 0., 0.)));
-        for edge in self.tiles.edges.iter() {
-            command_arena.push(RenderLine((edge.p1, edge.p2, edge.color)));
+        for tile in self.tiles.tiles.iter() {
+            for (l, r) in tile.edges() {
+                command_arena.push(RenderLine((l, r, Vec3::ONE)));
+            }
         }
-        return command_arena;
+        command_arena
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use crate::game::{Command, Game};
 extern crate sdl3;
 
 use glam::{Vec2, Vec3};
-use sdl3::pixels::Color;
 use sdl3::event::{Event, WindowEvent};
 use sdl3::keyboard::Keycode;
+use sdl3::pixels::Color;
 use std::time::Duration;
 
 fn vec3_to_color(normalized_color: &Vec3) -> Color {
@@ -19,7 +19,10 @@ fn vec3_to_color(normalized_color: &Vec3) -> Color {
 
 fn logical_coordinates(point: &Vec2, (window_w, window_h): (i32, i32)) -> (f32, f32) {
     let dimension = window_w.max(window_h) as f32;
-    (dimension * ((point.x + 1.) * 0.5) - (dimension - window_w as f32) * 0.5, dimension * ((-1. * point.y + 1.) * 0.5) - (dimension - window_h as f32) * 0.5)
+    (
+        dimension * ((point.x + 1.) * 0.5) - (dimension - window_w as f32) * 0.5,
+        dimension * ((-point.y + 1.) * 0.5) - (dimension - window_h as f32) * 0.5,
+    )
 }
 
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -42,10 +45,11 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
-                }
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
                 Event::Window {
                     win_event: WindowEvent::Resized(x, y),
                     ..
@@ -59,12 +63,12 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
         for command in command_arena.iter() {
             match command {
                 Command::Clear(normalized_color) => {
-                    let color = vec3_to_color(&normalized_color);
+                    let color = vec3_to_color(normalized_color);
                     canvas.set_draw_color(color);
                     canvas.clear();
                 }
                 Command::RenderLine((p1, p2, normalized_color)) => {
-                    let color = vec3_to_color(&normalized_color);
+                    let color = vec3_to_color(normalized_color);
                     let start = logical_coordinates(p1, window_size);
                     let end = logical_coordinates(p2, window_size);
                     canvas.set_draw_color(color);
